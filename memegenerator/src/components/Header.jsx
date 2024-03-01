11c46102c9c7e597f44095af18dd3c23a85120ef
Sadaquat Razzaq <sadaqatcustom@gmail.com>
2024-03-01 05:18:45 +0500

    created header component

--- memegenerator/src/components/Header.jsx
+++ memegenerator/src/components/Header.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import  React from 'react'
 
 
 export default function Header() {
     return (
-        <h1>header comp</h1>
+        <header className="header">
+            <img
+                src="./src/assets/troll-face.png"
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
     )
 }
